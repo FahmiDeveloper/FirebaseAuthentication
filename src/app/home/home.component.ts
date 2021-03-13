d62727924cbc172a0452841beda3da9ab6dd030c
FahmiDeveloper <coursangularfahmi@gmail.com>
2021-03-13 23:43:03 +0100

    new fixes part2

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../core/auth.service';
+import { Location } from '@angular/common';
+
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+  constructor(public authService: AuthService,private location : Location) { }
+
+  ngOnInit(): void {
+  }
+
+  logout(){
+    this.authService.doLogout()
+    .then((res) => {
+      this.location.back();
+    }, (error) => {
+      console.log("Logout error", error);
+    });
+  }
+
+}
