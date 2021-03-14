d310d2ba9baf0e185ad0a9a22d750de2983120e7
FahmiDeveloper <coursangularfahmi@gmail.com>
2021-03-14 11:38:53 +0100

    delete console

--- src/app/home/home.resolver.ts
+++ src/app/home/home.resolver.ts
@@ -15,7 +15,6 @@ export class HomeResolver implements Resolve<FirebaseUserModel> {
     return new Promise((resolve, reject) => {
       this.userService.getCurrentUser()
       .then(res => {
-        console.log(res)
         if(res.providerData[0].providerId == 'password'){
           user.image = 'https://via.placeholder.com/400x300';
           user.name = res.displayName;
